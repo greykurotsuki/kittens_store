af628f0b134bfbe1967b9be537926ff55d784200
greykurotsuki <50923540+greykurotsuki@users.noreply.github.com>
2021-12-13 11:22:26 +0200

    Update user_data.sh

--- terraform/eu-west-3/user_data.sh
+++ terraform/eu-west-3/user_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+# Some dependencies
  sudo apt-get update
  sudo apt-get install \
     ca-certificates \
@@ -12,9 +13,23 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+# Install docker
 sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io 
 
-sudo docker run -d -p 1234:1234 yuralagoyda/kittens-store:latest
+# Install docker-compose actual version
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Clone development branch of our repository
+git clone --branch development https://github.com/greykurotsuki/kittens_store.git
+cd kittens_store/
+
+# Run our application on port 1234 with database
+sudo docker-compose up -d
+sudo docker-compose run web bundle exec rake db:create db:migrate db:seed
+
 sleep 172800
 shutdown now
+#sudo docker run -p 1234:1234 --stop-timeout 300 yuralagoyda/kittens-store:latest
+#--stop-timeout 172800
